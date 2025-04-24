a6d9581065c8f4c6ad8d209acad01eea82ac2863
7ZyxS <julienambroggipro@gmail.com>
2025-04-24 05:07:53 +0200

    testingfile created need to resolve the Ace draw case

--- BlachJack/TesingFile.js
+++ BlachJack/TesingFile.js
@@ -2,14 +2,6 @@ import CardManager from "./src/gameLogic/CardsLogic.js";
 import GameLogic from "./src/gameLogic/GameLogic.js";
 
 
-let apiUrl = "https://deckofcardsapi.com/api/deck";
-let deck_id = "m6r1sntlan21";
-let playerHand = {};
-let dealerHand = {};
-let nextPlayerCardId = 1;
-let nextDealerCardId = 1;
-let backOfCard = `https://deckofcardsapi.com/static/img/back.png`
-
 const cardManager = new CardManager();
 cardManager.createNewDecks().then(() =>{
         cardManager.drawCard(cardManager.deck_id,2,"playerHand",false).then(() => {
